a47c9feb1cca6f5837c0accdf6024def2548633c
Luan Menezes <luanmenezes99@gmail.com>
2021-01-18 19:26:11 -0300

    first commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,23 @@
+import ProjectList from './app/ProjectList.js';
+
+class App {
+  static init() {
+    const activeProjectsList = new ProjectList('active');
+    const finishedProjectsList = new ProjectList('finished');
+    activeProjectsList.setSwitchHandlerFunction(
+      finishedProjectsList.addProject.bind(finishedProjectsList),
+    );
+    finishedProjectsList.setSwitchHandlerFunction(
+      activeProjectsList.addProject.bind(activeProjectsList),
+    );
+  }
+
+  static startAnalytics() {
+    const analyticsScript = document.createElement('script');
+    analyticsScript.src = 'assets/scripts/utility/analytics.js';
+    analyticsScript.defer = true;
+    document.head.append(analyticsScript);
+  }
+}
+
+App.init();
